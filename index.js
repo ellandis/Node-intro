487ebacce29f433b63cccd7340c3cc614443f4f3
ellandis <ellandis@crimson.ua.edu>
2021-09-14 07:43:48 -0400

    JS and Node
    You have to use the command line to run this example

---
+++ index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mysuperheroes = superheroes.random();
+var mysupervillains = supervillains.random();
+
+console.log(mysuperheroes + " versus the Evil " + mysupervillains);
